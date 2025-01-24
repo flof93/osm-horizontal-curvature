acbc4c80d885b370c5c7087bff9dc4031dd6e5f0
flof <ich.du.und.unsere.kuh@gmail.com>
2025-01-24 11:39:11 +0100

    Plot for Curvature of line poetry update

--- diplomarbeit.py
+++ diplomarbeit.py
@@ -1,6 +1,32 @@
 import curvy
+from matplotlib import pyplot as plt
 
-coords={"Wien":(15, 48, 17, 49)} # Koordinaten Wiens
-curvy = curvy.Curvy(*coords["Wien"], desired_railway_types=["tram"]) # Liest die Tramstrecken Wiens aus
-curvy.download_track_data()
-print(curvy)
+coords={"Wien":(16, 48, 16.75, 48.5)} # Koordinaten Wiens
+curvy = curvy.Curvy(*coords["Wien"], desired_railway_types=["tram"], download=True) # Liest die Tramstrecken Wiens aus
+#curvy.download_track_data()
+print("Download finished")
+
+# for i in curvy.railway_lines:
+#     line = i
+#     lower, upper = line.get_error_bounds()
+#
+
+def plt_line_curvature(line):
+    lower, upper = line.get_error_bounds()
+
+    fig, ax = plt.subplots(1, 1)
+
+    ax.plot(line.s, line.c)
+    ax.plot(line.s, lower)
+    ax.plot(line.s, upper)
+
+    ax.set_xlabel("Distances s [m]")
+    ax.set_ylabel("Curvature c [m]")
+    ax.grid()
+
+    plt.suptitle(line.name)
+    plt.show()
+
+
+line = curvy.railway_lines[0]
+plt_line_curvature(line)
