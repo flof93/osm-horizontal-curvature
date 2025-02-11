8f61c576e30a3fecef37ad5e259ed12eb8b48d38
flof <ich.du.und.unsere.kuh@gmail.com>
2025-02-11 13:40:15 +0100

    various plt-functions

--- diplomarbeit/diplomarbeit.py
+++ diplomarbeit/diplomarbeit.py
@@ -2,7 +2,9 @@ import curvy
 from matplotlib import pyplot as plt
 import scipy
 
-coords={"Wien":(16, 48, 16.75, 48.5)} # Koordinaten Wiens
+from curvy import Curvy
+
+coords={"Wien":(16, 48, 17, 48.5)} # Koordinaten Wiens
 #curvy.download_track_data()
 
 
@@ -11,7 +13,7 @@ coords={"Wien":(16, 48, 16.75, 48.5)} # Koordinaten Wiens
 #     lower, upper = line.get_error_bounds()
 #
 
-def plt_line_curvature(line, error_bounds = False, filter_savgol = False, savgol_win_l = 51, savgol_poly_o = 3):
+def plt_curvature(line, error_bounds = False, filter_savgol = False, savgol_win_l = 51, savgol_poly_o = 3):
 
     fig, ax = plt.subplots(1, 1)
 
@@ -34,12 +36,48 @@ def plt_line_curvature(line, error_bounds = False, filter_savgol = False, savgol
 
     plt.show()
 
+def plt_line(line):
+    fig, ax = plt.subplots(1, 1)
+    ax.plot(line.x,line.y,color=line.color)
+    ax.grid()
+    plt.suptitle(line.name)
+    plt.show()
+
+def plt_line_curvature(line):
+    fig, ax = plt.subplots(2, 1)
+    ax[0].plot(line.x, line.y, color=line.color)
+    ax[1].plot(line.s, line.c)
+    ax.grid()
+
+def plt_network(network : Curvy):
+    fig, ax = plt.subplots()
+    for line in network.railway_lines:
+        ax.plot(line.lon,line.lat,color=line.color)
+    ax.grid()
+    #ax.set_aspect('equal', adjustable='box')
+    plt.show()
+
+
+
+
+def calc_dist(line):
+    dist=[]
+    for i in line.s:
+        if i == 0:
+            dist.append(0)
+        else:
+            dist.append(line.s[i]-line.s[i-1])
+
+
 if __name__ == "__main__":
-    curvy = curvy.Curvy(*coords["Wien"],
+    wien_network = curvy.Curvy(*coords["Wien"],
                         desired_railway_types=["tram"],
                         download=True)  # Liest die Tramstrecken Wiens aus
     print("Download finished")
-    line_draw = curvy.railway_lines[0]
-    plt_line_curvature(line_draw)
+    line_draw = wien_network.railway_lines[0]
+    #plt_line(line_draw)
+    #plt_curvature(line_draw)
+    #plt_line_curvature(line_draw)
+    plt_network(wien_network)
 
 
