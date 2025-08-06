0b940ca15d5094ab05029b7f4e5235a21248bef0
Florian Förster <ich.du.und.unsere.kuh@gmail.com>
2025-08-06 17:47:02 +0200

    Added last module and todo

--- diplomarbeit/__init__.py
+++ diplomarbeit/__init__.py
@@ -1,3 +1,7 @@
 from diplomarbeit import utils
 from diplomarbeit import add_speeds_to_osm
 from diplomarbeit import get_osm_data
+from diplomarbeit import speeds
+
+
+#TODO: Tadej Anmerkung: Stationsabstände mitbetrachten - vergleichen mit Geschwindigkeiten (hat das eine Auswirkung auf die Geschwindigkeiten?)
