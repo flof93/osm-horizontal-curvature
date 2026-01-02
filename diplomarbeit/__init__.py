6ea617760716faa54592b726d44858a75860a2f3
Florian Förster <ich.du.und.unsere.kuh@gmail.com>
2026-01-02 17:30:45 +0100

    Added buildings.py
    calculates Building densities along routes

--- diplomarbeit/__init__.py
+++ diplomarbeit/__init__.py
@@ -2,6 +2,4 @@ from diplomarbeit import utils
 from diplomarbeit import add_speeds_to_osm
 from diplomarbeit import get_osm_data
 from diplomarbeit import speeds
-
-
-#TODO: Tadej Anmerkung: Stationsabstände mitbetrachten - vergleichen mit Geschwindigkeiten (hat das eine Auswirkung auf die Geschwindigkeiten?)
+from diplomarbeit import buildings
