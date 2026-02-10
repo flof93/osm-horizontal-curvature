237775ef1ecab2c048ac2c7a241f888e0998729e
Florian Förster <ich.du.und.unsere.kuh@gmail.com>
2026-02-10 16:15:34 +0100

    Renamed DATA_DICT, Added DATAFIELD_VARIABLENAME

--- diplomarbeit/constants.py
+++ diplomarbeit/constants.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-DATA_DICT = Path('./data')
+DATA_DIR = Path('~/Documents/Diplomarbeit/osm-horizontal-curvature/data').expanduser()
 
 STADIA_API = 'ccf6fac2-c284-43e8-b9f2-83716f034ba2'
 
@@ -33,7 +33,8 @@ COLUMN_NAMES_DE = {'curvature': 'Kurvigkeit [gon/km]',
                    'height_up': 'Aufstieg [m/km]',
                    'height_down': 'Abstieg [m/km]',
                    'rho_b': 'Bebauungsdichte [-]',
-                   'gauge': 'Spurweite [mm]'}
+                   'gauge': 'Spurweite [mm]',
+                   'Buffer_Width': 'Bufferbreite [m]'}
 
 COLUMN_NAMES_TEX_TWO_LINES = {'curvature': r'$\bar\gamma$' + '\n' + r'[gon/km]',
                     'avg_dist': r'$\bar l_{Hst}$' + '\n' + r'[m]',
@@ -41,7 +42,8 @@ COLUMN_NAMES_TEX_TWO_LINES = {'curvature': r'$\bar\gamma$' + '\n' + r'[gon/km]',
                     'height_up': r'$\bar s^{\uparrow}$' + '\n' + r'[\textperthousand]',
                     'height_down': r'$\bar s^{\downarrow}$' + '\n' + r'[\textperthousand]',
                     'rho_b': r'$\rho_B$' + '\n' + r'[1]',
-                    'gauge': r'$G$' + '\n' + r'[mm]'}
+                    'gauge': r'$G$' + '\n' + r'[mm]',
+                    'Buffer_Width': r'$b_{Buf}$' +'\n'+r'[m]'}
 
 COLUMN_NAMES_TEX = {'curvature': r'$\bar\gamma$ [gon/km]',
                     'avg_dist': r'$\bar l_{Hst}$ [m]',
@@ -49,7 +51,8 @@ COLUMN_NAMES_TEX = {'curvature': r'$\bar\gamma$ [gon/km]',
                     'height_up': r'$\bar s^{\uparrow}$ [\textperthousand]',
                     'height_down': r'$\bar s^{\downarrow}$ [\textperthousand]',
                     'rho_b': r'$\rho_B$ [1]',
-                    'gauge': r'$G$ [mm]'}
+                    'gauge': r'$G$ [mm]',
+                    'Buffer_Width': r'$b_{Buf}$ [m]'}
 
 COLUMN_NAMES_TEX_SHORT = {'curvature': r'$\bar\gamma$',
                           'avg_dist': r'$\bar l_{Hst}$',
@@ -57,4 +60,21 @@ COLUMN_NAMES_TEX_SHORT = {'curvature': r'$\bar\gamma$',
                           'height_up': r'$\bar s^{\uparrow}$',
                           'height_down': r'$\bar s^{\downarrow}$',
                           'rho_b': r'$\rho_B$',
-                          'gauge': r'$G$'}
+                          'gauge': r'$G$',
+                          'Buffer_Width': r'$b_{Buf}$'}
+
+DATAFIELD_VARIABLENAME = {'region': 'varRegion',
+                          'gauge': 'varGauge',
+                          'trip_speeds_new': 'varTripSpeed',
+                          'avg_dist': 'varAvgDist',
+                          'Buffer_Width': 'varBbuf',
+                          'Stadt': 'varStadt',
+                          'Country': 'varCountry',
+                          'GTFS-Daten': 'varGTFSLink',
+                          'GTFS-Provider': 'varGTFSProvider',
+                          'Nord': 'varNord',
+                          'Sued': 'varSued',
+                          'Ost': 'varOst',
+                          'West': 'varWest',
+                          'Anzahl Relationen': 'varnLinien',
+                          }
